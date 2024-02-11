2b0b8f6073be8fdf5d9f2f508246a4e973616054
HunterTTP <HunterTTP@gmail.com>
2024-02-11 09:32:22 -0500

    TemperatureHistoryCsv now gets created on startup

---
+++ open-temp-alert/src/repos/TemperatureRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace open_temp_alert.repos
+{
+    public class TemperatureRepository
+    {
+        private readonly string _pathToTemperatureHistoryCsv;
+
+        public TemperatureRepository()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            _pathToTemperatureHistoryCsv = Path.Combine(baseDirectory, "TemperatureHistory.csv");
+        }
+
+        public void EnsureTemperatureHistoryCsvExists()
+        {
+            if (!File.Exists(_pathToTemperatureHistoryCsv))
+            {
+                try
+                {
+                    var newTemperatureHistoryCsv = File.Create(_pathToTemperatureHistoryCsv);
+                    var historyCsvWriter = new StreamWriter(newTemperatureHistoryCsv);
+                    historyCsvWriter.WriteLine("Timestamp,CpuName,SensorName,Temperature");
+                    historyCsvWriter.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new FileLoadException("Could not create TemperatureHistoryCsv");
+                }
+            }
+        }
+    }
+}

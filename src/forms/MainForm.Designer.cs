ac2c22e76a5ed96ee77bd4032d45c149a641a26f
HunterTTP <HunterTTP@gmail.com>
2024-02-16 19:20:26 -0500

    Removed LiveChart

--- src/forms/MainForm.Designer.cs
+++ src/forms/MainForm.Designer.cs
@@ -37,34 +37,14 @@ namespace open_dust_monitor.forms
             DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            button1 = new Button();
             timer1 = new System.Timers.Timer();
             notifyIcon1 = new NotifyIcon(components);
-            panel1 = new Panel();
-            panel2 = new Panel();
-            button3 = new Button();
-            button2 = new Button();
-            label2 = new Label();
-            dataGridView1 = new DataGridView();
-            Metric = new DataGridViewTextBoxColumn();
             Value = new DataGridViewTextBoxColumn();
+            Metric = new DataGridViewTextBoxColumn();
+            dataGridView1 = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)timer1).BeginInit();
-            panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.AutoSize = true;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(22, 403);
-            button1.Margin = new Padding(2);
-            button1.Name = "button1";
-            button1.Size = new Size(99, 27);
-            button1.TabIndex = 1;
-            button1.Text = "Take Snapshot";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += SnapshotButton_Click;
             // 
             // timer1
             // 
@@ -83,135 +63,12 @@ namespace open_dust_monitor.forms
             notifyIcon1.Visible = true;
             notifyIcon1.MouseDown += SysTray_Click;
             // 
-            // panel1
+            // Value
             // 
-            panel1.ForeColor = Color.White;
-            panel1.Location = new Point(22, 59);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(506, 103);
-            panel1.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.DimGray;
-            panel2.Controls.Add(button3);
-            panel2.Controls.Add(button2);
-            panel2.Controls.Add(label2);
-            panel2.Location = new Point(-8, -13);
-            panel2.Margin = new Padding(0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(739, 41);
-            panel2.TabIndex = 3;
-            panel2.MouseDown += TitleBar_Drag;
-            // 
-            // button3
-            // 
-            button3.Anchor = AnchorStyles.Top;
-            button3.BackColor = Color.DimGray;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.White;
-            button3.Location = new Point(500, 12);
-            button3.Margin = new Padding(2);
-            button3.Name = "button3";
-            button3.Size = new Size(36, 26);
-            button3.TabIndex = 5;
-            button3.Text = "__";
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += CloseButton_Click;
-            // 
-            // button2
-            // 
-            button2.Anchor = AnchorStyles.Top;
-            button2.BackColor = Color.DimGray;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(540, 14);
-            button2.Margin = new Padding(2);
-            button2.Name = "button2";
-            button2.Size = new Size(32, 25);
-            button2.TabIndex = 4;
-            button2.Text = "X";
-            button2.TextAlign = ContentAlignment.BottomCenter;
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += MinimizeButton_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.FlatStyle = FlatStyle.Flat;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(15, 17);
-            label2.Margin = new Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(144, 21);
-            label2.TabIndex = 4;
-            label2.Text = "Open Dust Monitor";
-            label2.MouseDown += TitleBar_Drag;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.AllowUserToResizeColumns = false;
-            dataGridView1.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.ForeColor = Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(64, 64, 64);
-            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dataGridView1.BackgroundColor = Color.FromArgb(64, 64, 64);
-            dataGridView1.BorderStyle = BorderStyle.None;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle2.ForeColor = Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            dataGridView1.ColumnHeadersHeight = 34;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            dataGridView1.ColumnHeadersVisible = false;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Metric, Value });
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle3.ForeColor = Color.White;
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
-            dataGridView1.EnableHeadersVisualStyles = false;
-            dataGridView1.GridColor = Color.White;
-            dataGridView1.Location = new Point(22, 190);
-            dataGridView1.Margin = new Padding(2);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
-            dataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle4.ForeColor = Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            dataGridViewCellStyle5.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridViewCellStyle5.ForeColor = Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = Color.FromArgb(64, 64, 64);
-            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
-            dataGridView1.RowTemplate.DefaultCellStyle.BackColor = Color.FromArgb(64, 64, 64);
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.ShowCellToolTips = false;
-            dataGridView1.Size = new Size(523, 209);
-            dataGridView1.TabIndex = 4;
+            Value.HeaderText = "Value";
+            Value.MinimumWidth = 8;
+            Value.Name = "Value";
+            Value.ReadOnly = true;
             // 
             // Metric
             // 
@@ -220,52 +77,98 @@ namespace open_dust_monitor.forms
             Metric.Name = "Metric";
             Metric.ReadOnly = true;
             // 
-            // Value
+            // dataGridView1
             // 
-            Value.HeaderText = "Value";
-            Value.MinimumWidth = 8;
-            Value.Name = "Value";
-            Value.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = Color.Black;
+            dataGridViewCellStyle1.SelectionForeColor = Color.White;
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = Color.Black;
+            dataGridViewCellStyle2.SelectionForeColor = Color.White;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.ColumnHeadersHeight = 34;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Metric, Value });
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = Color.Black;
+            dataGridViewCellStyle3.SelectionForeColor = Color.White;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = Color.Black;
+            dataGridView1.Location = new Point(11, 11);
+            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = SystemColors.Control;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = Color.Black;
+            dataGridViewCellStyle4.SelectionForeColor = Color.White;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dataGridViewCellStyle5.BackColor = SystemColors.Control;
+            dataGridViewCellStyle5.ForeColor = Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = Color.Black;
+            dataGridViewCellStyle5.SelectionForeColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridView1.RowTemplate.DefaultCellStyle.BackColor = Color.White;
+            dataGridView1.RowTemplate.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = Color.Black;
+            dataGridView1.RowTemplate.DefaultCellStyle.SelectionForeColor = Color.White;
+            dataGridView1.RowTemplate.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ShowCellToolTips = false;
+            dataGridView1.Size = new Size(467, 301);
+            dataGridView1.TabIndex = 4;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(64, 64, 64);
-            ClientSize = new Size(566, 449);
-            Controls.Add(button1);
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(489, 321);
             Controls.Add(dataGridView1);
-            Controls.Add(panel1);
-            Controls.Add(panel2);
             ForeColor = Color.White;
-            FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(2);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Open Dust Monitor";
-            TopMost = true;
-            Load += MainForm_Load_1;
+            Load += MainForm_Load;
             ((System.ComponentModel.ISupportInitialize)timer1).EndInit();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         private System.Timers.Timer timer1;
 
-        private System.Windows.Forms.Button button1;
-
         #endregion
 
         private NotifyIcon notifyIcon1;
-        private Panel panel1;
-        private Panel panel2;
-        private Label label2;
-        private Button button3;
-        private Button button2;
         private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn Metric;
         private DataGridViewTextBoxColumn Value;
--- src/forms/MainForm.cs
+++ src/forms/MainForm.cs
@@ -1,31 +1,21 @@
-﻿using LiveCharts.Defaults;
-using open_dust_monitor.models;
+﻿using open_dust_monitor.models;
 using open_dust_monitor.services;
 using open_dust_monitor.src.Handler;
-using System.Runtime.InteropServices;
 using System.Timers;
 
 namespace open_dust_monitor.forms
 {
     public partial class MainForm : Form
     {
-        [DllImport("user32.dll")]
-        public static extern bool ReleaseCapture();
-
-        [DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-
         private readonly TemperatureService _temperatureService;
-        private readonly LiveCharts.WinForms.CartesianChart _temperatureChart;
 
         public MainForm()
         {
             InitializeComponent();
             _temperatureService = InstanceHandler.GetTemperatureService();
-            _temperatureChart = AddTemperatureChart();
         }
 
-        private void MainForm_Load_1(object sender, EventArgs e)
+        private void MainForm_Load(object sender, EventArgs e)
         {
             UpdateFormWithCpuInfo();
         }
@@ -40,20 +30,13 @@ namespace open_dust_monitor.forms
 
         private void SnapshotTimer_Elapse(object sender, ElapsedEventArgs e)
         {
-            UpdateFormWithCpuInfo();
-        }
-
-
-        private void SnapshotButton_Click(object sender, EventArgs e)
-        {
-            UpdateFormWithCpuInfo();
+            //UpdateFormWithCpuInfo();
         }
 
         private void UpdateFormWithCpuInfo()
         {
             var latestTemperatureSnapshot = _temperatureService.GetLatestTemperatureSnapshot();
             UpdateGridView(latestTemperatureSnapshot);
-            UpdateTemperatureChart(latestTemperatureSnapshot);
             AlertIfTemperatureIsOutsideThreshold();
         }
 
@@ -99,33 +82,6 @@ namespace open_dust_monitor.forms
                     MessageBoxOptions.DefaultDesktopOnly);
         }
 
-        private void UpdateTemperatureChart(TemperatureSnapshot snapshot)
-        {
-            if (ChartHandler.TemperatureSeries.Count >= 15)
-            {
-                ChartHandler.TemperatureSeries.RemoveAt(0);
-                ChartHandler.LoadSeries.RemoveAt(0);
-            }
-            ChartHandler.TemperatureSeries.Add(new DateTimePoint(snapshot.Timestamp, snapshot.CpuPackageTemperature));
-            ChartHandler.LoadSeries.Add(new DateTimePoint(snapshot.Timestamp, snapshot.CpuPackageUtilization));
-        }
-
-        private LiveCharts.WinForms.CartesianChart AddTemperatureChart()
-        {
-            var cartesianChart1 = ChartHandler.CreateTemperatureChart(panel1.Height, panel1.Width, _temperatureService.GetLatestTemperatureSnapshot());
-            this.panel1.Controls.Add(cartesianChart1);
-            return cartesianChart1;
-        }
-
-        private void TitleBar_Drag(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(this.Handle, 0xA1, 0x2, 0);
-            }
-        }
-
         private void MinimizeButton_Click(object sender, EventArgs e)
         {
             _temperatureService.StopTemperatureMonitoring();
@@ -152,17 +108,6 @@ namespace open_dust_monitor.forms
                 this.Visible = false;
                 this.ShowInTaskbar = false;
                 this.WindowState = FormWindowState.Minimized;
-            }
-        }
-
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                const int CS_DROPSHADOW = 0x20000;
-                CreateParams cp = base.CreateParams;
-                cp.ClassStyle |= CS_DROPSHADOW;
-                return cp;
             }
         }
     }
